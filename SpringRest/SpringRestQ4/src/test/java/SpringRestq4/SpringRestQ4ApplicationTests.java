494dcf252c4d912f28ad402ca218fc126216ee06
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-11-20 19:09:03 +0530

    Spring Rest Question 4

---
+++ SpringRest/SpringRestQ4/src/test/java/SpringRestq4/SpringRestQ4ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringRestq4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestQ4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
